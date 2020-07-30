eebb4b7c8d4d5b9e1b7d222896ac2d0c3189660a
kmarks2013 <kmarks2013@gmail.com>
2020-07-30 18:15:34 -0400

    adds basic render for blog container

---
+++ src/containers/BlogContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function BlogContainer() {
+    return (
+        <div id='tech-exp'>
+            <p>Blogs</p>
+        </div>
+    )
+}
+
+export default BlogContainer
