a052cb35e847df6709e83961b4e40d5553e3e41c
kmarks2013 <kmarks2013@gmail.com>
2020-07-30 18:16:29 -0400

    imports and renders blog container

--- src/containers/ContentContainer.js
+++ src/containers/ContentContainer.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import AboutMe from '../components/AboutMe'
 import Portfolio from './Portfolio'
+import BlogContainer from './BlogContainer'
 
 function ContentContainer() {
     return (
         <div className='content'>
             <AboutMe/>
             <Portfolio/>
+            <BlogContainer/>
         </div>
     )
 }
