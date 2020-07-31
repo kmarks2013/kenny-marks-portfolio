fa753d89ed00b286b53f2c4bf1612d926ab3381f
kmarks2013 <kmarks2013@gmail.com>
2020-07-31 09:54:03 -0400

    adds  twitter image

--- src/components/WebLinks.js
+++ src/components/WebLinks.js
@@ -6,6 +6,7 @@ function WebLinks() {
         <div id='contact'>
             <i className="fa fa-linkedin" aria-hidden="true"></i>
             <i className="fa fa-github" aria-hidden="true"></i>
+            <i className="fa fa-twitter" aria-hidden="true"></i>
         </div>
     )
 }
