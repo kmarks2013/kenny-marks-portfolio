4e245a232b10f38e639314e9bf257d37e14f9c39
kmarks2013 <kmarks2013@gmail.com>
2020-07-31 10:15:46 -0400

    adds a tag for linkedin

--- src/components/WebLinks.js
+++ src/components/WebLinks.js
@@ -3,8 +3,10 @@ import 'font-awesome/css/font-awesome.min.css'
 
 function WebLinks() {
     return (
-        <div id='contact'>
-            <i className="fa fa-linkedin" aria-hidden="true"></i>
+        <div id='contact' className='weblinks'>
+            <a className='link' href="https://www.linkedin.com/in/kmarks2013/" target="_blank" rel="noopener noreferrer">
+                <i className="fa fa-linkedin" aria-hidden="true"></i>
+            </a>GI
             <i className="fa fa-github" aria-hidden="true"></i>
             <i className="fa fa-twitter" aria-hidden="true"></i>
             <i className="fa fa-medium"></i>
