582f121bb700091fabcde6b81f5ba42a02e1c3e6
kmarks2013 <kmarks2013@gmail.com>
2020-07-31 10:16:23 -0400

    adds link for github" ;

--- src/components/WebLinks.js
+++ src/components/WebLinks.js
@@ -6,8 +6,10 @@ function WebLinks() {
         <div id='contact' className='weblinks'>
             <a className='link' href="https://www.linkedin.com/in/kmarks2013/" target="_blank" rel="noopener noreferrer">
                 <i className="fa fa-linkedin" aria-hidden="true"></i>
-            </a>GI
-            <i className="fa fa-github" aria-hidden="true"></i>
+            </a>
+            <a className='link' href="https://github.com/kmarks2013" target="_blank" rel="noopener noreferrer">
+                <i className="fa fa-github" aria-hidden="true"></i>
+            </a>
             <i className="fa fa-twitter" aria-hidden="true"></i>
             <i className="fa fa-medium"></i>
         </div>
