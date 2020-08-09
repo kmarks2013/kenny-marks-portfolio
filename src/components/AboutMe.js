3814d8a5f958c85d1641c79f22b9e9b511f333a0
kmarks2013 <kmarks2013@gmail.com>
2020-08-09 12:08:37 -0400

    adds bio header and basic text

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,6 +4,8 @@ function AboutMe() {
     return (
         <div id='about-me'>
             <h2 className='section-head'>About Me</h2>
+            <h3>Bio:</h3>
+            <p>Bio Text</p>
         </div>
     )
 }
