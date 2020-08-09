cf8d040eed676565b3b1fc33d728b7a4ac7babf5
kmarks2013 <kmarks2013@gmail.com>
2020-08-09 12:08:00 -0400

    updates about me  to h2 tag and adds class name

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,7 +3,7 @@ import React from 'react'
 function AboutMe() {
     return (
         <div id='about-me'>
-            <p> About me </p>
+            <h2 className='section-head'>About Me</h2>
         </div>
     )
 }
