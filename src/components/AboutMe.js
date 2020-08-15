323df32a29c3029eae0007d9d06414246f793a95
kmarks2013 <kmarks2013@gmail.com>
2020-08-15 18:56:22 -0400

    removes extra about me text

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -27,19 +27,6 @@ function AboutMe() {
                 </BackSide>
 
             </Flippy>
-            <h2 className='section-head'>About Me</h2>
-            <img className='about-img' src='../assets/headshot.jpg' alt='headshot'/>
-            <h3>Bio:</h3>
-            <p>Bio Text</p>
-            <h3>Technical Skills:</h3>
-            <p>Technical Text</p>
-            <h3>Experience:</h3>
-            <p>Experience Text</p>
-            <img className='about-img' src='../assets/fruitsnacks.jpeg' alt='fruitsnacks'/>
-            <h3>Interests:</h3>
-            <p>Interest Text</p>
-            <h3>Hobbies:</h3>
-            <p>Hobbies Text</p>
         </div>
     )
 }
