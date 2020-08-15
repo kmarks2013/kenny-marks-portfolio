e57b21a02a7f73665b1ecf8318021145aeaeb6d9
kmarks2013 <kmarks2013@gmail.com>
2020-08-15 18:50:40 -0400

    removes basic styling, and adds class name to about me images

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,14 +4,14 @@ function AboutMe() {
     return (
         <div id='about-me'>
             <h2 className='section-head'>About Me</h2>
-            <img src='../assets/headshot.jpg' alt='headshot'width='250px'/>
+            <img className='about-img' src='../assets/headshot.jpg' alt='headshot'/>
             <h3>Bio:</h3>
             <p>Bio Text</p>
             <h3>Technical Skills:</h3>
             <p>Technical Text</p>
             <h3>Experience:</h3>
             <p>Experience Text</p>
-            <img src='../assets/fruitsnacks.jpeg' alt='fruitsnacks'width='250px'/>
+            <img className='about-img' src='../assets/fruitsnacks.jpeg' alt='fruitsnacks'/>
             <h3>Interests:</h3>
             <p>Interest Text</p>
             <h3>Hobbies:</h3>
