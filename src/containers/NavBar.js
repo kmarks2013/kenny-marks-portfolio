ebd76c38ac47ad8fb9168061365a90ec749d1538
kmarks2013 <kmarks2013@gmail.com>
2020-08-23 17:11:34 -0400

    tests out on click

--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -12,7 +12,7 @@ function NavBar() {
     return (
         <div id='navbar'>
             <h1 className='nav-title'>{title}</h1>
-            <a href='#about-me'>About Me</a>
+            <a onClick={(e) => handleClick(e)} href='#about-me'>About Me</a>
             <a>My Work</a>
             <a href='#tech-writing'>Writings</a>
             <a>Twitter</a>
