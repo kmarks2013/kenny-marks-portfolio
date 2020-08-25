def3f0d91a2fa6a3d65d88aabaa90bc00a214c90
kmarks2013 <kmarks2013@gmail.com>
2020-08-25 11:42:19 -0400

    adds useState hook and initalizes current for index calls

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, {useState} from 'react'
 import LeftArrow from './LeftArrow'
 import ProjectSlide from './ProjectSlide'
 import RightArrow from './RightArrow'
 
 
 function Carousel({data}) {
+    const [current, setCurrent] = useState(0)
+
     return (
         <div className='carousel'>
         {console.log(data)}
