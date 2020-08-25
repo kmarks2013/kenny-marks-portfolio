d2e4183f48546e6d2d35c6795a6aaafc86bfd1db
kmarks2013 <kmarks2013@gmail.com>
2020-08-25 15:04:45 -0400

    renders out project details in ptags

--- src/components/carousel/ProjectSlide.js
+++ src/components/carousel/ProjectSlide.js
@@ -3,6 +3,16 @@ import React from 'react'
 const ProjectSlide = ({data,index}) => {
     return (
         <div className='projects' >
+          {
+            data.map((project, projectIndex) =>
+              <div>
+                <p>{project.title}</p>
+                <p>{project.description}</p>
+                <p>{project.url}</p>
+                <p>{project.demo}</p>
+              </div>
+            )
+          }
           <p>{data[index].title}</p>
         </div>
     )
