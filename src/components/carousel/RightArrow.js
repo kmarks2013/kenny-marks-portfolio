3af1e5b789e7073b73cb80eab409b64cb9af2eaa
kmarks2013 <kmarks2013@gmail.com>
2020-09-01 18:04:05 -0400

    removes p tag

--- src/components/carousel/RightArrow.js
+++ src/components/carousel/RightArrow.js
@@ -3,7 +3,6 @@ import React from 'react'
 const RightArrow = () => {
     return (
         <div>
-            <p> I will be aright arrow!</p>
             <i className='fa fa-angle-right fa-3x' aria-hidden='true'></i>
         </div>
     )
