fd27e794a8f5bfef2e0b2c78ad0553bfa4f56cba
kmarks2013 <kmarks2013@gmail.com>
2020-09-01 18:01:55 -0400

    adds left arrow image

--- src/components/carousel/LeftArrow.js
+++ src/components/carousel/LeftArrow.js
@@ -4,6 +4,7 @@ const LeftArrow = () => {
     return (
         <div>
             <p>I will be a left arrow!</p>
+            <i className='fa fa-angle-left fa-3x' aria-hidden='true'></i>
         </div>
     )
 }
