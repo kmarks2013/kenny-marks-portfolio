7af8beda4acaed2f77e9e5283f83d2169a9a7e95
kmarks2013 <kmarks2013@gmail.com>
2020-09-01 18:03:26 -0400

    deletes p tag

--- src/components/carousel/LeftArrow.js
+++ src/components/carousel/LeftArrow.js
@@ -3,7 +3,6 @@ import React from 'react'
 const LeftArrow = () => {
     return (
         <div>
-            <p>I will be a left arrow!</p>
             <i className='fa fa-angle-left fa-3x' aria-hidden='true'></i>
         </div>
     )
