a8340b61266238ceac718830c2775de6bb7a8bb1
kmarks2013 <kmarks2013@gmail.com>
2020-09-01 18:19:54 -0400

    adds right arrow class name

--- src/components/carousel/RightArrow.js
+++ src/components/carousel/RightArrow.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const RightArrow = () => {
     return (
-        <div>
+        <div className='right-arrow'>
             <i className='fa fa-angle-right fa-3x' aria-hidden='true'></i>
         </div>
     )
