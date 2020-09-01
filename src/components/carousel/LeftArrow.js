b19e35c25e0d6f9ca7d1dedc448a008cfd64aa56
kmarks2013 <kmarks2013@gmail.com>
2020-09-01 17:56:22 -0400

    adds basic render to left arrow

---
+++ src/components/carousel/LeftArrow.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LeftArrow = () => {
+    return (
+        <div>
+            <p>I will be a left arrow!</p>
+        </div>
+    )
+}
+
+export default LeftArrow
