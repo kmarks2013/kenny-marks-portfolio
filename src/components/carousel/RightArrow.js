57bb67e681a5ebfeef3f9c00f00a59d06a700a4f
kmarks2013 <kmarks2013@gmail.com>
2020-09-01 17:57:02 -0400

    adds basic render to right arrow

---
+++ src/components/carousel/RightArrow.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RightArrow = () => {
+    return (
+        <div>
+            <p> I will be aright arrow!</p>
+        </div>
+    )
+}
+
+export default RightArrow
