62459d96622b617f56e01db3ce7f718391c30afa
kmarks2013 <kmarks2013@gmail.com>
2020-09-03 14:24:53 -0400

    updates blogcard creation and export call

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Blog = () => {
+const BlogCard = () => {
     return (
         <div>
             ill render the whole Blog
@@ -8,4 +8,4 @@ const Blog = () => {
     )
 }
 
-export default Blog
+export default BlogCard
