e40f2c977921f640ccafe62ad792f28db606cdf8
kmarks2013 <kmarks2013@gmail.com>
2020-09-03 14:28:44 -0400

    renders blog info and image in blog card

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import BlogInfo from './blogcard/BlogInfo'
+import BlogImage from './blogcard/BlogImage'
 
 const BlogCard = () => {
     return (
         <div>
             ill render the whole Blog
+            <BlogInfo/>
+            <BlogImage/>
         </div>
     )
 }
