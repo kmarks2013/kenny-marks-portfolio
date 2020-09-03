62f96332b02fb9301d6ac3aef0ffc827b34ae4da
kmarks2013 <kmarks2013@gmail.com>
2020-09-03 14:03:10 -0400

    fetchs medium posts and console.logs them

--- src/containers/BlogContainer.js
+++ src/containers/BlogContainer.js
@@ -1,6 +1,14 @@
 import React, {useState, useEffect} from 'react'
 
 function BlogContainer() {
+    useEffect(() => {
+        fetch("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40kmarks2013")
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+    },[])
+
     return (
         <div id='blog'>
             <p>Blogs</p>
