a3bbffa6eca6087341df1460389eb9d892c0fd14
kmarks2013 <kmarks2013@gmail.com>
2020-09-03 14:09:53 -0400

    updates fetch to set the stae of medium posts

--- src/containers/BlogContainer.js
+++ src/containers/BlogContainer.js
@@ -7,7 +7,9 @@ function BlogContainer() {
         fetch("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40kmarks2013")
             .then(res => res.json())
             .then(data => {
-                console.log(data)
+                let posts = data.items.filter(article => article.categories.length > 0)
+                setMediumPosts(posts)
+                console.log(posts)
             })
     },[])
 
