f10f1cbfad393216d9c4596209e89c451356b0e6
kmarks2013 <kmarks2013@gmail.com>
2020-09-03 14:19:17 -0400

    adds blogcard file with basic render

---
+++ src/components/blogcard/BlogCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BlogCard = () => {
+    return (
+        <div>
+            renders the rest of the blog information!
+        </div>
+    )
+}
+
+export default BlogCard
