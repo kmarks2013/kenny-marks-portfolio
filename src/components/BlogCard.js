87a5cd2fd19100fa50e90153cc6d59cddbd37e6a
kmarks2013 <kmarks2013@gmail.com>
2020-09-03 14:43:34 -0400

    console.logs blog in blogcard

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -2,10 +2,11 @@ import React from 'react'
 import BlogInfo from './blogcard/BlogInfo'
 import BlogImage from './blogcard/BlogImage'
 
-const BlogCard = () => {
+const BlogCard = ({blog}) => {
     return (
         <div>
             ill render the whole Blog
+            {console.log(blog)}
             <BlogInfo/>
             <BlogImage/>
         </div>
