e3d06615169f0c6ec574807c2e028163f29c3007
kmarks2013 <kmarks2013@gmail.com>
2020-09-03 14:17:45 -0400

    adds blogcard folder and Blog Image file

---
+++ src/components/blogcard/BlogImage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BlogImage = () => {
+    return (
+        <div>
+            i will show the image of a blog!
+        </div>
+    )
+}
+
+export default BlogImage
