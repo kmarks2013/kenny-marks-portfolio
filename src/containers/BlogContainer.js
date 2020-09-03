de06995a69db0e5e6ddcafdb02f714c110ef1cb6
kmarks2013 <kmarks2013@gmail.com>
2020-09-03 14:08:16 -0400

    adds state for medium posts

--- src/containers/BlogContainer.js
+++ src/containers/BlogContainer.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react'
 
 function BlogContainer() {
+    const [mediumPosts, setMediumPosts] = useState([])
+
     useEffect(() => {
         fetch("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40kmarks2013")
             .then(res => res.json())
