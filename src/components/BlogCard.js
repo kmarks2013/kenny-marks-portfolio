b3aa4d5fcd66673e965f50747623994abe16fd54
kmarks2013 <kmarks2013@gmail.com>
2020-09-03 22:41:36 -0400

    moves blog info below blog image

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -5,8 +5,8 @@ import BlogImage from './blogcard/BlogImage'
 const BlogCard = ({blog}) => {
     return (
         <div className='blog-card'>
-            <BlogInfo blog={blog}/>
             <BlogImage src={blog.thumbnail}/>
+            <BlogInfo blog={blog}/>
         </div>
     )
 }
