c13b70a1f81730a8e9f3f73020d701a27050f922
kmarks2013 <kmarks2013@gmail.com>
2020-09-08 19:08:22 -0400

    removes div from blog image

--- src/components/blogcard/BlogImage.js
+++ src/components/blogcard/BlogImage.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const BlogImage = ({src}) => {
     return (
-        <div>
+        <>
             <img className='blog-thumb' src={src} alt='img' />
-        </div>
+        </>
     )
 }
 
