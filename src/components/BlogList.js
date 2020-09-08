5747fea55e2c59aebb8744ad459f24200435f1b4
kmarks2013 <kmarks2013@gmail.com>
2020-09-08 17:49:57 -0400

    creates renderBlogs function that maps out all blogs and console.logs them

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -2,6 +2,11 @@ import React from 'react'
 import BlogCard from './BlogCard'
 
 const BlogList = ({blogs}) => {
+
+    const renderBlogs = blogs.map(blog=> {
+        console.log(blog)
+    })
+
     return (
         <div>
             all blogs will render here!
