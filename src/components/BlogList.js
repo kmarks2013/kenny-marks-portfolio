d37ceab51d14911c28071cfc80c373ec45cf3904
kmarks2013 <kmarks2013@gmail.com>
2020-09-08 17:46:27 -0400

    renders the first blog

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import BlogCard from './BlogCard'
 
 const BlogList = ({blogs}) => {
     return (
         <div>
             all blogs will render here!
+            <BlogCard blog={blogs[0]}/>
         </div>
     )
 }
