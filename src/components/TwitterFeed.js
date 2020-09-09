12b529c445dcb266f9a3c300149c90dcce9e4978
kmarks2013 <kmarks2013@gmail.com>
2020-09-09 18:17:02 -0400

    adds Twitter feed

--- src/components/TwitterFeed.js
+++ src/components/TwitterFeed.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import {TwitterTimelineEmbed} from 'react-twitter-embed'
 
 function TwitterFeed() {
     return (
         <div id="twitter">
             <p>Tweets</p>
+            <TwitterTimelineEmbed
+                sourceType='widget'
+                screenName='kmarks2013'
+                options={{height:400, width: 200}}
+                noScrollbar
+                noHeader
+                theme='dark'
+            />
         </div>
     )
 }
