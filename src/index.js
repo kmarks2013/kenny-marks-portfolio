82096e51f1d0eb245a767c1a8e34fd01e61d223d
kmarks2013 <kmarks2013@gmail.com>
2020-09-10 18:12:50 -0400

    imports font to index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import './fonts/8-BIT WONDER.TTF'
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom'
 
