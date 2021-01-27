226a05a9d9e6aa147264bbccdbc5f82ccf8b119d
kmarks2013 <kmarks2013@gmail.com>
2021-01-26 19:48:41 -0500

    adds a div for hte left links

--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -14,8 +14,10 @@ function NavBar() {
         <div id='navbar'>
             <h3 className='nav-title'>{title}</h3>
             <div className='nav-links'>
+                <div className='nav-links-left'>
                 <NavLink className='nav-link' scroll={el => el.scrollIntoView({ behavior: 'smooth', block: 'end' })} to='/#about-me' onClick={(e) => handleClick(e)} >About Me</NavLink>
                 <NavLink className='nav-link' scroll={el => el.scrollIntoView({ behavior: 'smooth', block: 'end' })} to='/#my-work' onClick={(e) => handleClick(e)} >My Work</NavLink>
+                </div>
                 <NavLink className='nav-link' scroll={el => el.scrollIntoView({ behavior: 'smooth', block: 'end' })} to='/#blog' onClick={(e) => handleClick(e)} >My Blog</NavLink>
                 <NavLink className='nav-link' scroll={el => el.scrollIntoView({ behavior: 'smooth', block: 'end' })} to='/#twitter' onClick={(e) => handleClick(e)} >Twitter</NavLink>
                 <NavLink className='nav-link' scroll={el => el.scrollIntoView({ behavior: 'smooth', block: 'end' })} to='/#external' onClick={(e) => handleClick(e)} >External</NavLink>
